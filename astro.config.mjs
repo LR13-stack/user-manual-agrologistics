ff3ca6a3e1a39614a77d2f6d08186dbab3f91ebe
Luis Romero Pretel <marco2002rp@gmail.com>
2024-12-04 17:00:51 -0500

    Add GitHub Pages deployment workflow and update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://LR13-stack.github.io',
+    base: 'user-manual-agrologistics',
+});
