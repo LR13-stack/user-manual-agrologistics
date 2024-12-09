fa1455e16b1355c3f8048d1e4cda554370937159
Luis Romero Pretel <marco2002rp@gmail.com>
2024-12-09 02:13:00 -0500

    Add Tailwind CSS integration and update layout styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://LR13-stack.github.io',
-    base: 'user-manual-agrologistics',
+  site: 'https://LR13-stack.github.io',
+  base: 'user-manual-agrologistics',
+  integrations: [tailwind()],
 });
